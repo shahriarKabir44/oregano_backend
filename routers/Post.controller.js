765fa76bcbbc40a33992b6f9cdbecfcad33c1672
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-06-20 01:41:01 +0600

    shipped to firebase from local storage

--- routers/Post.controller.js
+++ routers/Post.controller.js
@@ -1,26 +1,10 @@
 let PostController = require('express').Router()
 const Post = require('../schemas/post')
-const Tags = require('../schemas/tags')
-const multer = require('multer');
-const fs = require('fs')
-const path = require('path');
+
 const AvailableItem = require('../schemas/availableItem');
 
-const upload = multer()
 
-PostController.post('/upload', upload.array(), (req, res) => {
-    let { postid, postedby, postedon, type, filename } = req.headers
-    let dir = path.join(__dirname, '..', `static/upload/${postedby}/${postedon}`)
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-    fs.writeFile(`${dir}/${filename}.${type}`, req.body.file.replace(/^data:image\/jpeg;base64,/, "").replace(/^data:image\/jpg;base64,/, "").replace(/^data:image\/png;base64,/, ""), 'base64', function (err) {
-        if (err) console.log(err);
-        res.send({ data: `http://192.168.43.90:3000/upload/${postedby}/${postedon}/${filename}.${type}` });
 
-    });
-    //res.send({ data: "test" })
-})
 
 PostController.post('/removeTodayTags', (req, res) => {
     AvailableItem.deleteMany({ userId: req.body.userId })
@@ -60,16 +44,11 @@ PostController.post('/getTagsOfToday', (req, res) => {
     })
 })
 
-PostController.post('/updatePostImages', (req, res) => {
-    Post.findByIdAndUpdate(req.body.postId, { images: req.body.images })
-        .then(data => {
-            res.send({ data: 1 })
-        })
-})
+
 PostController.get("/isItemAvailable/:itemName/:ownerId", (req, res) => {
     let currentDay = Math.floor(((new Date()) * 1) / (24 * 3600 * 1000))
     let { itemName, ownerId } = req.params
-    console.log(req.params)
+
     AvailableItem.findOne({
         $and: [
             { userId: ownerId },
@@ -78,7 +57,6 @@ PostController.get("/isItemAvailable/:itemName/:ownerId", (req, res) => {
         ]
     })
         .then((row, err) => {
-            console.log(row)
             if (!row) {
                 res.send({ data: { isAvailable: 0 } })
             }
@@ -94,17 +72,7 @@ PostController.get("/isItemAvailable/:itemName/:ownerId", (req, res) => {
 })
 PostController.post('/createPost', async (req, res) => {
     let newPost = new Post(req.body)
-    console.log(req.body)
     await newPost.save()
-    let newPostId = newPost._id
-    let tagList = JSON.parse(req.body.tags)
-    for (let tag of tagList) {
-        let newTag = new Tags({
-            tagName: tag,
-            postId: newPostId,
-        })
-        await newTag.save()
-    }
 
     res.send({ data: newPost })
 })
